66f8def12cf267f1a33a7c2ef0e19b69d246e322
Bogumil Kaczkowski <bogumil@tkx305.genome.gsc.riken.jp>
2019-08-05 15:45:51 +0900

    add enhancer GRanges to "data"

--- data-raw/F5_genomic_regions/enhancers/hg38_enhancer_regions.R
+++ data-raw/F5_genomic_regions/enhancers/hg38_enhancer_regions.R
@@ -84,3 +84,5 @@ rtracklayer::export.bed(object = all_enh ,
 
 save(all_enh, file ="data-raw/F5_genomic_regions/enhancers/FANTOM5_hg38_and_hg19_enhancers_2019_10_05_GRanges.RData")
 
+enhancers = all_enh
+usethis::use_data( enhancers , internal = FALSE)
