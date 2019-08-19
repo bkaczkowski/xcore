194635572a5020e4f8c4382b6a6c7580d2880c72
Bogumil Kaczkowski <bogumil@tkx305.genome.gsc.riken.jp>
2019-08-19 17:50:03 +0900

    minor edits

--- data-raw/repeats/repetitive_element_annotations.R
+++ data-raw/repeats/repetitive_element_annotations.R
@@ -1,7 +1,8 @@
 
+# Bogumil Kaczkowski August 19, 2019
+# A script is to make a BED file with repeats with Dfam database
 
 dfam_nrph = data.table::fread("~/projects/resources/Dfam/hg38_dfam.3.1.nrph.hits.gz")
-
 
 dfam_nrph_bed = data.frame( chr = dfam_nrph$`#seq_name` ,
                             start = dfam_nrph$`ali-st` -1 , # adjusting "start" to the BED format which is 0-based
@@ -9,11 +10,11 @@ dfam_nrph_bed = data.frame( chr = dfam_nrph$`#seq_name` ,
                             name = dfam_nrph$family_name,
                             score = -log10(dfam_nrph$`e-value`),
                             strand = dfam_nrph$strand)
+
 # adjusting "start" and "end" of the alighnments to the BED format for "-" strand
 minus_strand = dfam_nrph_bed$strand == "-"
 dfam_nrph_bed$start[ minus_strand ] = dfam_nrph$`ali-en` [ minus_strand] -1  # adjusting "start" to the BED format which is 0-based
 dfam_nrph_bed$end  [ minus_strand ] = dfam_nrph$`ali-st` [ minus_strand]
-
 
 # scaling the score to 1-1000 range
 dfam_nrph_bed$score [ is.infinite(dfam_nrph_bed$score) ] = 320
@@ -26,6 +27,8 @@ hist(dfam_nrph_bed$score)
 options(scipen = 999)
 write.table( dfam_nrph_bed, file = "data-raw/repeats/hg38_dfam.3.1.nrph.hits.bed",
              col.names = F, row.names = F, quote = F, sep = "\t" )
+#write.table( dfam_nrph_bed, file = "~/projects/resources/Dfam/hg38_dfam.3.1.nrph.hits.bed",
+#             col.names = F, row.names = F, quote = F, sep = "\t" )
 options(scipen = 15)
 
 system( "gzip data-raw/repeats/hg38_dfam.3.1.nrph.hits.bed")
