2ff6b03c97b1f5c38b157ac26c8fce04229b6277
Bogumil Kaczkowski <bogumil@tkx305.genome.gsc.riken.jp>
2019-08-22 15:59:19 +0900

    organize and streamline the dpi annotation

--- data-raw/F5_genomic_regions/DPIs/f5_dpi_promoters.R
+++ data-raw/F5_genomic_regions/DPIs/f5_dpi_promoters.R
@@ -17,35 +17,20 @@ dpi$SYMBOL_F5_annot    = sub( " .*", "", dpi_annot$Gene_symbol )
 dpi$ENTREZID_F5_annot  = sub( " .*", "", dpi_annot$GeneID     )
 dpi$distance_F5_annot  = dpi_annot$Distance
 
-# ROADMAP
-roadmap_hg19 = rtracklayer::import.bed("~/projects/resources/roadmap/EpigenomeRoadmapDHS_bed6.bed", colnames = c("chrom", "start", "end", "name", "score"))
-hg19_hg38_chain = rtracklayer::import.chain("~/projects/resources/liftOver_chains/hg19ToHg38.over.chain")
-roadmap = rtracklayer::liftOver(roadmap_hg19, chain = hg19_hg38_chain )
-roadmap = unlist(roadmap)
-dpi$roadmap = ""
-hits = findOverlaps(dpi , roadmap )
-dpi$roadmap[hits@from] = roadmap$name[hits@to]
-rm(roadmap_hg19, roadmap ); gc()
+# GENCODE 31 annotation
+gencode = rtracklayer::import.gff(con = "~/projects/resources/gencode/gencode31/gencode.v31.annotation.gff3.gz")
+dpi_gencode = xcore::gencode_nearest_promoter_same_strand(regions = dpi , gencode = gencode, cut_off_distance = 500)
+dpi$SYMBOL_gencode    = dpi_gencode$nearest_symbol
+library(org.Hs.eg.db)
+select_first = function(x) {x[1]}
+dpi$ENTREZID_gencode  = as.character( lapply ( mapIds(org.Hs.eg.db, dpi$SYMBOL_gencode, 'ENTREZID' , 'SYMBOL'  ) , select_first))
+dpi$ENTREZID_gencode[ dpi$ENTREZID_gencode =="NULL"] =""
+dpi$ENTREZID_gencode[ is.na(dpi$ENTREZID_gencode) ] =""
+dpi$gene_type_gencode = as.factor(dpi_gencode$nearest_gene_type)
+dpi$distance_gencode  = dpi_gencode$nearest_distance
 
-dpi$ep300 = ""
-ep300  = rtracklayer::import.bed("~/projects/resources/remap/remap2018_EP300_nr_macs2_hg38_v1_2.bed.gz" )
-hits = findOverlaps(dpi ,ep300)
-dpi$ep300[hits@from] = "EP300"
-dpi$ep300 = as.factor(dpi$ep300)
-
-#promoters = dpi
-#usethis::use_data( promoters , internal = FALSE, overwrite = TRUE)
-
-data("enhancers", package = "xcore")
-dpi$enhancer = ""
-hits = findOverlaps(dpi ,enhancers)
-dpi$enhancer[hits@from] = enhancers$name[hits@to]
-
-dfam  = rtracklayer::import.bed("data-raw/repeats/hg38_dfam.3.1.nrph.hits.bed.gz" )
-dpi$repeat_dfam = ""
-hits = findOverlaps(dpi , dfam )
-dpi$repeat_dfam [hits@from] = dfam$name[hits@to]
-
+# UCSC.hg38.knownGene annotation
+library(GenomicRanges)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 uscs_knownGene = GenomicFeatures::asGFF(TxDb.Hsapiens.UCSC.hg38.knownGene)
 uscs_knownGene = uscs_knownGene[ uscs_knownGene$type == "mRNA"]
@@ -63,15 +48,87 @@ dpi$ENTREZID_uscs [hits@from]   =  uscs_knownGene_promoters$Parent [hits@to]
 dpi$distance_uscs [hits@from]   =  hits@elementMetadata$distance
 library(org.Hs.eg.db)
 select_first = function(x) {x[1]}
-dpi$SYMBOL_uscs  =  lapply ( mapIds(org.Hs.eg.db, dpi$ENTREZID_uscs, 'SYMBOL' , 'ENTREZID' ) , select_first)
+dpi$SYMBOL_uscs  = as.character( lapply ( mapIds(org.Hs.eg.db, dpi$ENTREZID_uscs, 'SYMBOL' , 'ENTREZID' ) , select_first))
+dpi$SYMBOL_uscs[ dpi$SYMBOL_uscs =="NULL"] =""
 
 too_far = dpi$distance_uscs > 500
 
-dpi$ENTREZID_uscs[ too_far ]  = NA
-dpi$distance_uscs[ too_far ]  = NA
-dpi$SYMBOL_uscs  [ too_far ]  = NA
-
+dpi$ENTREZID_uscs[ too_far ]  = ""
+dpi$SYMBOL_uscs  [ too_far ]  = ""
 
 promoters = dpi
+promoters$distance_F5_annot = NULL
+promoters$distance_gencode = NULL
+promoters$distance_uscs = NULL
+
 usethis::use_data( promoters , internal = FALSE, overwrite = TRUE)
 
+# ROADMAP
+roadmap = rtracklayer::import.bed("~/projects/resources/roadmap/EpigenomeRoadmapDHS_hg38.bed", colnames = c("chrom", "start", "end", "name", "score"))
+dpi$roadmap = ""
+hits = findOverlaps(dpi , roadmap )
+dpi$roadmap[hits@from] = roadmap$name[hits@to]
+rm(roadmap_hg19, roadmap ); gc()
+
+#EP300
+dpi$ep300 = ""
+ep300  = rtracklayer::import.bed("~/projects/resources/remap/remap2018_EP300_nr_macs2_hg38_v1_2.bed.gz" )
+hits = findOverlaps(dpi ,ep300)
+dpi$ep300[hits@from] = "EP300"
+dpi$ep300 = as.factor(dpi$ep300)
+
+#ENHANCERS
+data("enhancers", package = "xcore")
+dpi$enhancer = ""
+hits = findOverlaps(dpi ,enhancers)
+dpi$enhancer[hits@from] = enhancers$name[hits@to]
+
+#DFAM
+dfam  = rtracklayer::import.bed("data-raw/repeats/hg38_dfam.3.1.nrph.hits.bed.gz" )
+dpi$repeat_dfam = ""
+hits = findOverlaps(dpi , dfam )
+dpi$repeat_dfam [hits@from] = dfam$name[hits@to]
+
+dpi = xcore::gencode_two_direction_annotator(regions = dpi, gencode = gencode)
+
+promoters_detailed = dpi
+usethis::use_data( promoters_detailed , internal = FALSE, overwrite = TRUE)
+
+# some checks, stats and overview
+table(dpi$SYMBOL_F5_annot == dpi$SYMBOL_gencode)
+table(dpi$ENTREZID_F5_annot == dpi$ENTREZID_gencode)
+table(dpi$ENTREZID_uscs == dpi$ENTREZID_gencode)
+
+table(dpi$SYMBOL_F5_annot != "")
+table(dpi$SYMBOL_gencode != "")
+table(dpi$SYMBOL_uscs != "")
+
+table(dpi$ENTREZID_F5_annot != "")
+table(dpi$ENTREZID_gencode != "")
+table(dpi$ENTREZID_uscs    != "")
+
+length(unique(dpi$SYMBOL_F5_annot))
+length(unique(dpi$SYMBOL_gencode))
+length(unique(dpi$SYMBOL_uscs))
+
+length(unique(dpi$ENTREZID_F5_annot))
+length(unique(dpi$ENTREZID_gencode))
+length(unique(dpi$ENTREZID_uscs))
+
+table( dpi$SYMBOL_gencode != "", dpi$annotation == "promoter")
+
+s = dpi$SYMBOL_gencode != ""
+table(dpi$SYMBOL_gencode[s]  == dpi$symbol[s])
+
+dpi [ dpi$SYMBOL_gencode  != dpi$symbol & s ]
+
+table(dpi$SYMBOL_F5_annot[s] == dpi$symbol[s])
+table(dpi$SYMBOL_F5_annot[s] == dpi$SYMBOL_gencode[s])
+
+table(dpi$SYMBOL_uscs[s]     == dpi$symbol[s])
+table(dpi$SYMBOL_uscs[s]     == dpi$SYMBOL_gencode[s])
+
+table(dpi$SYMBOL_uscs[s]     == dpi$SYMBOL_F5_annot[s])
+
+#there are some annotation cases that are confusing
+dpi [ dpi$SYMBOL_gencode  != dpi$symbol & s ,c(3,6,10,18)]
