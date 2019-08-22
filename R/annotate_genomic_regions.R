a2dd912f54a94fb3d2e4207c6a5bae8cace41a61
Bogumil Kaczkowski <bogumil@tkx305.genome.gsc.riken.jp>
2019-08-22 13:44:05 +0900

    add function gencode_nearest_promoter_same_strand

--- R/annotate_genomic_regions.R
+++ R/annotate_genomic_regions.R
@@ -168,6 +168,28 @@ gencode_nearest_promoter_both_strands = function( regions, gencode ) {
   regions
 }
 
+#' Annotate with Nearest Promoter on the Same Strand
+#' @param regions query genomic regions as GRanges object
+#' @param gencode complete Gencode annotation as GRanges object, e.g. obrained by rtracklayer::import.gff(con = "gencode.v31.annotation.gff3.gz") ,  (gencode[ gencode$type == "transcript"] is also OK)
+#' @return GRanges with new columns added to the feature metadata: mcols(regions)
+#' @importFrom GenomicRanges promoters mcols
+#' @importFrom S4Vectors DataFrame
+#' @export
+gencode_nearest_promoter_same_strand = function( regions, gencode , cut_off_distance = 500) {
+
+  gencode_promoters   = GenomicRanges::promoters( gencode[ gencode$type == "transcript"] , upstream = 0, downstream = 0, use.names=TRUE)
+
+  regions = gencode_one_direction_distanceToNearest( regions = regions, gencode = gencode_promoters)
+
+  if( !is.null( cut_off_distance )){
+    too_far = regions$nearest_distance > cut_off_distance
+    regions$nearest_symbol     [ too_far ]  = ""
+    regions$nearest_gene_type  [ too_far ]  = ""
+  }
+
+  regions
+}
+
 #' Simplify Gencode Annotation
 #' @param annotation character vector with Gencode annotation that can be used to grep() labels as: "intron", "exon", "three_prime_UTR", "five_prime_UTR", "promoter"
 #' @return character vector with simplified labels
