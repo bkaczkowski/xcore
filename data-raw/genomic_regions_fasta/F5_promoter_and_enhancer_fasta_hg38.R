c463fd6a3259e43cb0f066bda44448ae6ab12f59
Bogumil Kaczkowski <bogumil@tkx305.genome.gsc.riken.jp>
2019-11-20 11:21:49 +0900

    minor edits

--- data-raw/genomic_regions_fasta/F5_promoter_and_enhancer_fasta_hg38.R
+++ data-raw/genomic_regions_fasta/F5_promoter_and_enhancer_fasta_hg38.R
@@ -9,8 +9,8 @@ promoters_1.1kb = GenomicRanges::trim(promoters_1.1kb)
 promoters_1.1kb_seq    = BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38 , promoters_1.1kb)
 names(promoters_1.1kb_seq) = promoters_1.1kb$name
 
-Biostrings::writeXStringSet(promoters_1.1kb_seq, "data-raw/F5_genomic_regions/promoters_1.1kb_seq.fasta")
-save(promoters_1.1kb_seq    , file = "data-raw/F5_genomic_regions/promoters_1.1kb_seq.rda" )
+Biostrings::writeXStringSet(promoters_1.1kb_seq, "data-raw/genomic_regions_fasta/promoters_1.1kb_seq.fasta")
+save(promoters_1.1kb_seq    , file = "data-raw/genomic_regions_fasta/promoters_1.1kb_seq.rda" )
 #usethis::use_data( promoters_1.1kb_seq   , internal = FALSE, overwrite = TRUE)
 
 #FANTOM5 ENHANCERS
@@ -23,6 +23,6 @@ enhancers_1.1kb = GenomicRanges::trim(enhancers_1.1kb)
 enhancers_1.1kb_seq = BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38 , enhancers_1.1kb)
 table( width(enhancers_1.1kb_seq) == width( enhancers_1.1kb)) # checking if the lenght of the sequences is the same (same order)
 names(enhancers_1.1kb_seq) = enhancers_1.1kb$name
-Biostrings::writeXStringSet(enhancers_1.1kb_seq, "data-raw/F5_genomic_regions/enhancers_1.1kb_seq.fasta")
+Biostrings::writeXStringSet(enhancers_1.1kb_seq, "data-raw/genomic_regions_fasta/enhancers_1.1kb_seq.fasta")
 
-save(enhancers_1.1kb_seq , file = "data-raw/F5_genomic_regions/enhancers_1.1kb_seq.rda" )
+save(enhancers_1.1kb_seq , file = "data-raw/genomic_regions_fasta/enhancers_1.1kb_seq.rda" )
