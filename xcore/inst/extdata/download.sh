89d4db2613be97a959054aec9bbe1e957db70cf4
Maciej Migdał <mcjmigdal@gmail.com>
2021-07-14 09:33:42 +0200

    extend promoters signatures analysis to lncRNA and enhancers

--- xcore/inst/extdata/download.sh
+++ xcore/inst/extdata/download.sh
@@ -53,7 +53,12 @@ downloadDataset https://github.com/Boyle-Lab/Blacklist/raw/master/lists/hg38-bla
 # Protein Atlas data
 downloadDataset https://www.proteinatlas.org/download/proteinatlas.tsv.zip
 
-# CAGE peaks expression tables
+# CAGE peaks (promoters) expression tables
 # The format is weird so I reprocessed it a bit
 downloadDataset https://fantom.gsc.riken.jp/5/datafiles/reprocessed/hg38_v8/extra/CAGE_peaks_expression/hg38.fair+new_CAGE_peaks_phase1and2_tpm.osc.txt.gz
+zcat hg38_fair+new_CAGE_peaks_phase1and2_tpm.osc.txt.gz | grep -v -e "^##" -e "^01STAT" -e "^02STAT" | gzip > t && mv t hg38_fair+new_CAGE_peaks_phase1and2_tpm.osc.txt.gz
+
+# enhancers expression tables
+# The format is weird so I reprocessed it a bit
+downloadDataset https://fantom.gsc.riken.jp/5/datafiles/reprocessed/hg38_v8/extra/enhancer/F5.hg38.enhancers.expression.tpm.matrix.gz
 zcat hg38_fair+new_CAGE_peaks_phase1and2_tpm.osc.txt.gz | grep -v -e "^##" -e "^01STAT" -e "^02STAT" | gzip > t && mv t hg38_fair+new_CAGE_peaks_phase1and2_tpm.osc.txt.gz
