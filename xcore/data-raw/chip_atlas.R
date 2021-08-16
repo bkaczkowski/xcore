af3b9e9bd1d3b118f27679cdb633cb86b83f1dc2
Migdal <mmigdal@iimcb.gov.pl>
2021-08-16 14:35:18 +0200

    Signatures technical biases and noise vs biological signal (#8)
    * add chip atlas processing script to package
    
    * descrive chip_atlas data
    
    * include extended instructions for ChIP-Atlas download in download.sh script; those include post download processing
    
    * add code formatting to data.R
    
    * add dependencies to DESCRIPTION
    
    * document
    
    * fix typo
    
    * fix typo in chip_meta documentation; run devtools::document
    
    * add LazyDataCompression flag to DESCRIPTION
    
    * technical_biases_and_noise_vs_biological_signal
    
    * lint data-raw
    
    * redownload remap datasets
    
    * download encode blacklist for hg38
    
    * rename promoters data set to promoters_f5 to avoid name clash with IRanges::promoters
    
    * re-export pipe operator
    
    * stage into_signatures.Rmd analysis
    
    * rename data-raw/promoters.R t data-raw/promoters_f5.R
    
    * remove ambiguous experiments from chip atlas
    
    * run analysis only on a core subset of promoters
    
    * cleaning promoters analysis freeze
    
    * download proteinatlas and cage peaks expression data
    
    * FANTOM5 tissue specificty score
    
    * reorder labels
    
    * extend promoters signatures analysis to lncRNA and enhancers
    
    * fix enhancers interaction matrix construction such that presence/absence matrix is returned
    
    * add data with remap and chip_atlas metadata
    
    * rename enhancers to enhancers_f5 for consistiency with promoters_f5
    
    * histogram with summary
    
    * add tau, protein atlas, encode blacklist and ensembl soft mask metadata to promoter_f5 and enhancers_f5 objects
    
    * experiments correlation heatmap plot
    
    * interaction matrix simplification function using majority voting
    
    * fix study ids in metadata
    
    * stash analysis; those are stashed in the repository for development purpose
    
    * add CIS-BP TF binding domain classifiction to metadata
    
    * fix roxygen comments
    
    * document
    
    * download FANTOM CAT data
    
    * modify plotExperimentHeatmap to take distance matrix instead of calculating one
    
    * implement functions for collapsing experiments based on hieralchical clustering
    
    * plot subtree
    
    * towards finalizing analyses
    
    * document
    
    * write test for collapisngExperiments
    
    * fix plotSubtree
    
    * add data.table as package dependency, so that it is attached when loading package
    
    * stage current concluding markdown on signatures and experiment collapsing
    
    * fix plotExperimentHeatmap
    
    * comment out PAM clustering on ChIP-Atlas data; comment out unused code

--- xcore/data-raw/chip_atlas.R
+++ xcore/data-raw/chip_atlas.R
@@ -14,37 +14,55 @@
 devtools::load_all()
 
 # 1c) read in the peak into R
-chip_atlas_file <- 
+chip_atlas_file <-
   system.file("inst", "extdata", "chip_atlas_hg38.Oth.ALL.05.AllAg.AllCell_SRX_only.bed.gz", package = "xcore")
 chip_atlas <- rtracklayer::import(chip_atlas_file)
 chip_atlas$score <- as.integer(chip_atlas$score)
+
+# remove ambiguous records
+ambiguous_tf <- c("5-hmC", "5-mC", "8-Hydroxydeoxyguanosine", "Biotin", "BMI",
+                  "BrdU", "Cas9", "Cyclobutane", "MethylCap", "O-GlcNAc",
+                  "Pan-acetyllysine", "pFM2", "RTA", "SVS-1", "EBNA1", "EBNA2",
+                  "EBNA3", "EBV-ZEBRA", "AML1-ETO", "VSV-G", "MLL-AF4",
+                  "MLL-AF6", "Hepatitis", "HIV", "KSHV", "MCPV", "Epitope tags",
+                  "GFP")
+ambiguous_srx <-
+  read.delim(
+    file = system.file("inst", "extdata", "experimentList_TF_hg38.txt", package = "xcore"),
+    sep = "\t",
+    header = FALSE,
+    quote = "\"") %>%
+  dplyr::filter(V4 %in% ambiguous_tf) %>% # V4 == TF column
+  dplyr::pull(V1) # V1 == experiment ID
+chip_atlas <- chip_atlas[! chip_atlas$name %in% ambiguous_srx, ]
+
 unique_srxIDs <- unique(chip_atlas$name)
 unique_srxIDs <- unique_srxIDs[order(unique_srxIDs)]
 
 # 2) get F5 promoter region
-data("promoters", package = "xcore")
-promoters_ext500 <- promoters
-GenomicRanges::start(promoters_ext500) <- GenomicRanges::start (promoters) - 500
-GenomicRanges::end(promoters_ext500) <- GenomicRanges::end(promoters) + 500
+data("promoters_f5", package = "xcore")
+promoters_ext500 <- promoters_f5
+GenomicRanges::start(promoters_ext500) <- GenomicRanges::start(promoters_f5) - 500
+GenomicRanges::end(promoters_ext500) <- GenomicRanges::end(promoters_f5) + 500
 
 data("enhancers", package = "xcore")
 enhancers_ext500 <- enhancers
 GenomicRanges::start(enhancers_ext500) <- GenomicRanges::start(enhancers) - 500
 GenomicRanges::end(enhancers_ext500) <- GenomicRanges::end(enhancers) + 500
 
-rm(enhancers, promoters)
+rm(enhancers, promoters_f5)
 gc()
 
 # 3) create intersection matrises
-overlap_mat_promoters <- matrix(data = 0L, 
-				nrow = length(promoters_ext500), 
-				ncol = length(unique_srxIDs))
+overlap_mat_promoters <- matrix(data = 0L,
+                                nrow = length(promoters_ext500),
+                                ncol = length(unique_srxIDs))
 colnames(overlap_mat_promoters) <- unique_srxIDs
 rownames(overlap_mat_promoters) <- promoters_ext500$name
 
-overlap_mat_enhancers <- matrix(data = 0L, 
-				nrow = length(enhancers_ext500), 
-				ncol = length(unique_srxIDs))
+overlap_mat_enhancers <- matrix(data = 0L,
+                                nrow = length(enhancers_ext500),
+                                ncol = length(unique_srxIDs))
 colnames(overlap_mat_enhancers) <- unique_srxIDs
 rownames(overlap_mat_enhancers) <- enhancers_ext500$name
 
@@ -57,17 +75,17 @@ for (j in 1:length(unique_srxIDs)){
   total_number_of_peaks[j] <- length(query_tf)
 
   hits <- GenomicRanges::findOverlaps(promoters_ext500, query_tf)
-  hits <- S4Vectors::DataFrame(from = hits@from, 
-			       to = hits@to, 
-			       score = query_tf$score[hits@to])
+  hits <- S4Vectors::DataFrame(from = hits@from,
+                               to = hits@to,
+                               score = query_tf$score[hits@to])
   hits <- hits[order(hits$score, decreasing = TRUE), ]
   hits <- hits[! duplicated(hits$from), ]
   overlap_mat_promoters[hits$from, j] <- query_tf$score[hits$to]
 
   hits <- GenomicRanges::findOverlaps(enhancers_ext500, query_tf)
-  hits <- S4Vectors::DataFrame(from = hits@from, 
-			       to = hits@to, 
-			       score = query_tf$score[hits@to])
+  hits <- S4Vectors::DataFrame(from = hits@from,
+                               to = hits@to,
+                               score = query_tf$score[hits@to])
   hits <- hits[order(hits$score, decreasing = T), ]
   hits <- hits[! duplicated(hits$from), ]
   overlap_mat_enhancers[hits$from, j] <- query_tf$score[hits$to]
@@ -79,11 +97,11 @@ rm(hits, query_tf, j, chip_atlas) # , export_dir)
 gc()
 
 # 4) Meta data for the chipseq experiments
-sxr_meta_file <- 
+sxr_meta_file <-
   system.file("inst", "extdata", "experimentList_TF_hg38.txt", package = "xcore")
-sxr_meta <- read.delim(file = sxr_meta_file, 
-		      sep ="\t", 
-		      header = FALSE, 
+sxr_meta <- read.delim(file = sxr_meta_file,
+		      sep ="\t",
+		      header = FALSE,
 		      quote = "\"")
 dim(sxr_meta)
 sxr_meta <- sxr_meta[sxr_meta$V1 %in% unique_srxIDs, ]
@@ -115,21 +133,21 @@ promoters_ext500_for_entrez_collapsing <- promoters_ext500[! empty]
 overlap_mat_promoters_for_entrez_collapsing <- overlap_mat_promoters[! empty, ]
 
 max_score_symbol <- function(x) {
-  tapply(X = x, 
-	 INDEX = promoters_ext500_for_gene_collapsing$SYMBOL, 
+  tapply(X = x,
+	 INDEX = promoters_ext500_for_gene_collapsing$SYMBOL,
 	 FUN = max)
 }
 max_score_entrez <- function(x) {
-  tapply(X = x, 
-	 INDEX = promoters_ext500_for_entrez_collapsing$ENTREZID, 
+  tapply(X = x,
+	 INDEX = promoters_ext500_for_entrez_collapsing$ENTREZID,
 	 FUN = max)
 }
 
 overlap_mat_symbol <- apply(X = overlap_mat_promoters_for_gene_collapsing,
-                            MARGIN = 2, 
+                            MARGIN = 2,
 			    FUN = max_score_symbol)
 overlap_mat_entrez <- apply(X = overlap_mat_promoters_for_entrez_collapsing,
-                            MARGIN = 2, 
+                            MARGIN = 2,
 			    FUN = max_score_entrez)
 
 sxr_meta$number_of_geneSymbols_with_peak <- colSums(overlap_mat_symbol > 0)
@@ -139,22 +157,35 @@ sxr_meta$total_number_of_peaks <- total_number_of_peaks
 # Export data
 chip_atlas_entrez <- Matrix::drop0(overlap_mat_entrez)
 chip_atlas_symbol <- Matrix::drop0(overlap_mat_symbol)
-chip_atlas_meta <- sxr_meta
+# chip_atlas_meta <- sxr_meta
 
+# promters to absence/presence matrix
 chip_atlas_promoters <- Matrix::sparseMatrix(
-  dims = c(nrow(overlap_mat_promoters), ncol(overlap_mat_promoters)), 
-  i = {}, 
+  dims = c(nrow(overlap_mat_promoters), ncol(overlap_mat_promoters)),
+  i = {},
   j = {})
 colnames(chip_atlas_promoters) <- colnames(overlap_mat_promoters)
 rownames(chip_atlas_promoters) <- rownames(overlap_mat_promoters)
 for (j in 1:ncol(chip_atlas_promoters)) {
   chip_atlas_promoters[, j] <- as.logical(overlap_mat_promoters[, j])
 }
-chip_atlas_enhancers <- Matrix::drop0(overlap_mat_enhancers)
+chip_atlas_promoters <- as(chip_atlas_promoters, "dgCMatrix") # how remap is stored
 
+# enhancers to absence/presence matrix
+chip_atlas_enhancers <- Matrix::sparseMatrix(
+  dims = c(nrow(overlap_mat_enhancers), ncol(overlap_mat_enhancers)),
+  i = {},
+  j = {})
+colnames(chip_atlas_enhancers) <- colnames(overlap_mat_enhancers)
+rownames(chip_atlas_enhancers) <- rownames(overlap_mat_enhancers)
+for (j in 1:ncol(chip_atlas_enhancers)) {
+  chip_atlas_enhancers[, j] <- as.logical(overlap_mat_enhancers[, j])
+}
+chip_atlas_enhancers <- as(chip_atlas_enhancers, "dgCMatrix") # how remap is stored
+
+# usethis::use_data
 usethis::use_data(chip_atlas_entrez, internal = FALSE, overwrite = TRUE)
 usethis::use_data(chip_atlas_symbol, internal = FALSE, overwrite = TRUE)
-usethis::use_data(chip_atlas_meta, internal = FALSE, overwrite = TRUE)
+# usethis::use_data(chip_atlas_meta, internal = FALSE, overwrite = TRUE)
 usethis::use_data(chip_atlas_promoters, internal = FALSE, overwrite = TRUE)
 usethis::use_data(chip_atlas_enhancers, internal = FALSE, overwrite = TRUE)
-
--- xcore/data-raw/remap.R
+++ xcore/data-raw/remap.R
@@ -10,38 +10,42 @@ devtools::load_all()
 # 1b) Get ReMap All peak set
 # wget http://tagc.univ-mrs.fr/remap/download/remap2018/hg38/MACS/remap2018_all_macs2_hg38_v1_2.bed.gz
 # zcat remap2018_all_macs2_hg38_v1_2.bed.gz | awk 'OFS="\t" {print $1,$2,$3,$4,$5,$6}' > remap2018_all_macs2_hg38_v1_2.bed6.bed
-remap_input <- 
-  system.file("inst", "extdata", "remap2020_all_macs2_hg38_v1_0.bed.gz", package = "xcore") 
+remap_input <-
+    system.file("inst", "extdata", "remap2020_all_macs2_hg38_v1_0.bed.gz", package = "xcore")
 remap <- rtracklayer::import(remap_input)
-remap$name <- gsub("\\s+", "", remap$name) # remove white spaces from features names 
+remap$name <- gsub("\\s+", "", remap$name) # remove white spaces from features names
 trans_factors <- unique(remap$name)
 trans_factors <- trans_factors[order(trans_factors)]
 
 # 2) get F5 promoter region
-data("promoters", package = "xcore")
-promoters_ext500 <- promoters
-GenomicRanges::start(promoters_ext500) <- GenomicRanges::start(promoters) - 500
-GenomicRanges::end(promoters_ext500) <- GenomicRanges::end(promoters) + 500
+data("promoters_f5", package = "xcore")
+promoters_ext500 <- promoters_f5
+GenomicRanges::start(promoters_ext500) <- GenomicRanges::start(promoters_f5) - 500
+GenomicRanges::end(promoters_ext500) <- GenomicRanges::end(promoters_f5) + 500
 
 data("enhancers", package = "xcore")
 enhancers_ext500 <- enhancers
 GenomicRanges::start(enhancers_ext500) <- GenomicRanges::start(enhancers) - 500
 GenomicRanges::end(enhancers_ext500) <- GenomicRanges::end(enhancers) + 500
 
-rm(enhancers, promoters) ; gc()
+rm(enhancers, promoters_f5) ; gc()
 
 # 3) create intersection matrix
 
-overlap_mat_promoters <- matrix(0L, nrow = length(promoters_ext500), ncol = length(trans_factors))
+overlap_mat_promoters <- matrix(data = 0L,
+                                nrow = length(promoters_ext500),
+                                ncol = length(trans_factors))
 colnames(overlap_mat_promoters) <- trans_factors
 rownames(overlap_mat_promoters) <- promoters_ext500$name
 
-overlap_mat_enhancers <- matrix(0L, nrow = length(enhancers_ext500), ncol = length(trans_factors))
+overlap_mat_enhancers <- matrix(data = 0L,
+                                nrow = length(enhancers_ext500),
+                                ncol = length(trans_factors))
 colnames(overlap_mat_enhancers) <- trans_factors
 rownames(overlap_mat_enhancers) <- enhancers_ext500$name
 
 
-for (j in 1:length(trans_factors)){
+for (j in 1:length(trans_factors)) {
   query_tf <- remap[remap$name == trans_factors[j]]
 
   hits <- GenomicRanges::findOverlaps(promoters_ext500, query_tf)
