3773bf59a98b819f99321cfbdbef78686d2c5e99
Migdal <mcjmigdal@gmail.com>
2021-11-15 17:53:03 +0100

    document prepareCountsForRegression; remove whitespaces

--- xcore/R/processing.R
+++ xcore/R/processing.R
@@ -1,16 +1,24 @@
-#' Process count matrix for modeling # TODO
-#' 
+#' Process count matrix for expression modeling
+#'
+#' Expression counts are processed using \link[edgeR]{edgeR} following
+#' \href{https://bioconductor.org/packages/release/bioc/vignettes/edgeR/inst/doc/edgeRUsersGuide.pdf}{User's Guide}.
+#' Shortly, counts for each sample are filtered for lowly expressed promoters,
+#' normalized for the library size and transformed into counts per million (CPM).
+#' Optionally, CPM are log2 transformed with addition of pseudo count. Basal
+#' level expression is calculated by averaging \code{base_lvl} samples
+#' expression values.
+#'
 #' @param counts matrix of read counts.
 #' @param groups factor giving the experimental group for each sample.
 #' @param base_lvl string indicating level in \code{groups} corresponding to
 #'   basal expression level. The reference samples to which expression change
 #'   will be compared.
-#' @param log2 logical flag indicating if counts should be log2(counts per 
+#' @param log2 logical flag indicating if counts should be log2(counts per
 #'   million) should be returned.
 #' @param pseudo_count integer count to be added before taking log2.
 #' @param drop_base_lvl logical flag indicating if \code{base_lvl} samples
 #'   should be dropped from resulting MultiAssayExperiment object.
-#'   
+#'
 #' @return MultiAssayExperiment object with two experiments:
 #'   \describe{
 #'     \item{U}{matrix giving expression values averaged over basal level samples}
@@ -32,7 +40,7 @@ prepareCountsForRegression <- function(counts,
   deglist <- deglist[keep, , keep.lib.sizes=FALSE] # if not keep some genes have 0 sd
   deglist <- edgeR::calcNormFactors(deglist)
   cpm <- edgeR::cpm(deglist)
-  
+
   if (log2) {
     cpm <- log2(cpm + pseudo_count)
   }
@@ -41,11 +49,11 @@ prepareCountsForRegression <- function(counts,
     data = rowMeans(cpm[, groups == base_lvl, drop = FALSE]),
     ncol = 1L,
     dimnames = list(rownames(cpm), "u"))
-  
+
   ymask <- if (drop_base_lvl) { groups != base_lvl } else { rep(TRUE, length(groups)) }
   MultiAssayExperiment::MultiAssayExperiment(
     experiments = MultiAssayExperiment::ExperimentList(
-      U = U, 
+      U = U,
       Y = cpm[, ymask, drop = FALSE]))
 }
 
@@ -58,7 +66,7 @@ addSignatures <- function(mae, ..., intersect_rows = TRUE) {
   if (is.null(ex_names) || any(ex_names == "")) {
     stop("All experiments must be named")
   }
-  
+
   mae_map <- MultiAssayExperiment::sampleMap(mae)
   ex_map <-
     lapply(ex, function(x)
@@ -67,19 +75,19 @@ addSignatures <- function(mae, ..., intersect_rows = TRUE) {
   newListMap <- c(MultiAssayExperiment::mapToList(mae_map),
                   IRanges::SplitDataFrameList(ex_map))
   newSampleMap <- MultiAssayExperiment::listToMap(newListMap)
-  
+
   newExperimentList <- c(MultiAssayExperiment::experiments(mae), ex)
-  
+
   # TODO preserve mae colData
   newColData <- S4Vectors::DataFrame(row.names = newSampleMap[["primary"]])
-  
+
   new_mae <- BiocGenerics:::replaceSlots(object = mae,
                                          ExperimentList = newExperimentList,
                                          sampleMap = newSampleMap,
                                          colData = newColData)
-  
+
   if (intersect_rows) new_mae <- MultiAssayExperiment::intersectRows(new_mae)
-  
+
   return(new_mae)
 }
 
@@ -102,21 +110,21 @@ addSignatures <- function(mae, ..., intersect_rows = TRUE) {
 #'
 #' @importFrom GenomicRanges start end findOverlaps
 #' @importFrom Matrix sparseMatrix
-#' 
+#'
 #' @examples
 #' a <- GenomicRanges::GRanges(
 #'   seqnames = c("chr20", "chr4"),
 #'   ranges = IRanges::IRanges(
-#'     start = c(62475984L, 173530220L), 
-#'     end = c(62476001L, 173530236L)), 
+#'     start = c(62475984L, 173530220L),
+#'     end = c(62476001L, 173530236L)),
 #'   strand = c("-", "-"),
-#'   name = c("hg19::chr20:61051039..61051057,-;hg_188273.1", 
-#'            "hg19::chr4:174451370..174451387,-;hg_54881.1"))  
-#' b <- GenomicRanges::GRanges( 
+#'   name = c("hg19::chr20:61051039..61051057,-;hg_188273.1",
+#'            "hg19::chr4:174451370..174451387,-;hg_54881.1"))
+#' b <- GenomicRanges::GRanges(
 #'   seqnames = c("chr4", "chr20"),
 #'   ranges = IRanges::IRanges(
-#'     start = c(173530229L, 63864270L), 
-#'     end = c(173530236L, 63864273L)), 
+#'     start = c(173530229L, 63864270L),
+#'     end = c(173530236L, 63864273L)),
 #'   strand = c("-", "-"),
 #'   name = c("HAND2", "GATA5"))
 #' getInteractionMatrix(a, b)
@@ -129,10 +137,10 @@ getInteractionMatrix <- function(a, b, ext = 500, count = FALSE) {
   stopifnot("b must have 'name' attribute in it's metadata" = "name" %in% colnames(GenomicRanges::elementMetadata(b)))
   stopifnot("ext must be numeric larger or equal to 0" = is.numeric(ext) & ext >= 0)
   stopifnot("count must be TRUE or FALSE" = isTRUEorFALSE(count))
-  
+
   GenomicRanges::start(a) <- GenomicRanges::start(a) - ext
   GenomicRanges::end(a) <- GenomicRanges::end(a) + ext
-  
+
   hits <- GenomicRanges::findOverlaps(a, b)
   i <- factor(x = a$name[hits@from], levels = unique(a$name))
   j <- factor(x = b$name[hits@to], levels = unique(b$name))
@@ -143,10 +151,10 @@ getInteractionMatrix <- function(a, b, ext = 500, count = FALSE) {
     dims = c(nlevels(i), nlevels(j)),
     dimnames = list(levels(i), levels(j))
   )
-  
+
   if (! count) {
     attr(overlap_mat, "x") <- rep(1, length(attr(overlap_mat, "x")))
   }
-  
+
   return(overlap_mat)
 }
