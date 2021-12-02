9ebda3d58890e3cfbcd44ddb4a42315cb1f3f249
Migdal <mcjmigdal@gmail.com>
2021-12-02 13:29:00 +0100

    add tests for stats

--- xcore/tests/testthat/test_stats.R
+++ xcore/tests/testthat/test_stats.R
@@ -1,12 +1,12 @@
 test_that("ridgePvals", {
-  ridge_mod <- ridge::linearRidge(formula = mpg ~ ., 
-                                  data = mtcars, 
-                                  lambda = 2.747, 
+  ridge_mod <- ridge::linearRidge(formula = mpg ~ .,
+                                  data = mtcars,
+                                  lambda = 2.747,
                                   scaling = "scale")
   ridge_pvals <- ridge::pvals(ridge_mod)
   xcore_pvals <- ridgePvals(x = scale(mtcars[, -1]),
                               y = mtcars[, 1] - mean(mtcars[, 1]), # ridge subtract avg from y
-                              beta = ridge_mod$coef[, 1], 
+                              beta = ridge_mod$coef[, 1],
                               lambda = 2.747, standardizex = FALSE)
   testthat::expect_equal(
     round(as.numeric(ridge_pvals$se), 10),
@@ -17,4 +17,245 @@ test_that("ridgePvals", {
   testthat::expect_equal(
     round(as.numeric(ridge_pvals$pval), 10),
     round(as.numeric(xcore_pvals$pval), 10))
+})
+
+test_that("modelGeneExpression", {
+  base_lvl <- "00hr"
+  design <- matrix(
+    data = c(1, 0,
+             1, 0,
+             1, 0,
+             0, 1,
+             0, 1,
+             0, 1),
+    ncol = 2,
+    nrow = 6,
+    byrow = TRUE,
+    dimnames = list(colnames(rinderpest_mini), c("00hr", "24hr"))
+  )
+  mae <- prepareCountsForRegression(
+    counts = rinderpest_mini,
+    design = design,
+    base_lvl = base_lvl)
+  mae <- addSignatures(mae, remap = remap_mini)
+  yname <- "Y"
+  uname <- "U"
+  xnames <- c("remap")
+
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = 1L
+    ),
+    "mae must be an instance of class 'MultiAssayExperiment'"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = LETTERS
+    ),
+    "yname must be a length one character"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = LETTERS
+    ),
+    "uname must be a length one character"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = yname
+    ),
+    "yname must be distinct from uname"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = 1:3
+    ),
+    "xnames must be a character vector"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = c(yname, uname)
+    ),
+    "xnames must be distinct from yname and uname"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = letters
+    ),
+    "yname, uname and xnames must match mae names"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames
+    ),
+    "remap can not contain zero variance signatures"
+  )
+  mae <- filterSignatures(mae)
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = LETTERS
+    ),
+    "design must be a matrix"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = matrix(data = NA, dimnames = list("foo", "bar"))
+    ),
+    "design rownames must correspond to mae\\[\\[yname\\]\\] columns"
+  )
+  fake_design <- design + 1
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = fake_design
+    ),
+    "each sample in design can be assigned only to one group"
+  )
+  fake_design <- fake_design - fake_design
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = fake_design
+    ),
+    "at least one sample in design must be assigned to a group"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = 1L
+    ),
+    "standardize must be TRUE or FALSE"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = TRUE,
+      parallel = 1L
+    ),
+    "parallel must be TRUE or FALSE"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = TRUE,
+      parallel = TRUE,
+      pvalues = 1L
+    ),
+    "pvalues must be TRUE or FALSE"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = TRUE,
+      parallel = TRUE,
+      elaborate_replicates_avg = TRUE,
+      precalcmodels = LETTERS
+    ),
+    "precalcmodels must be a list"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = TRUE,
+      parallel = TRUE,
+      elaborate_replicates_avg = TRUE,
+      precalcmodels = list()
+    ),
+    "precalcmodels elements names must be included in xnames"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = TRUE,
+      parallel = TRUE,
+      elaborate_replicates_avg = TRUE,
+      precalcmodels = list(remap = list("foo"))
+    ),
+    "precalcmodels must contain only objects of class 'cv.glmnet'"
+  )
+  testthat::expect_error(
+    modelGeneExpression(
+      mae = mae,
+      yname = yname,
+      uname = uname,
+      xnames = xnames,
+      design = design,
+      standardize = TRUE,
+      parallel = TRUE,
+      elaborate_replicates_avg = TRUE,
+      precalcmodels = list(
+        remap = list(
+          bar = structure(
+            list(glmnet.fit = list(beta = "bar")),
+            class = "cv.glmnet")
+          )
+        )
+    ),
+    "precalcmodels is not compatible with mae"
+  )
+
+  set.seed(1234512)
+  res <- suppressWarnings(suppressMessages(modelGeneExpression(
+    mae = mae,
+    yname = yname,
+    uname = uname,
+    xnames = xnames)))
+  load(system.file("inst", "extdata", "modelGeneExpression_test_data.rda", package = "xcore"))
+  testthat::expect_equal(res$pvalues, modelGeneExpression_test_pvalues)
+  testthat::expect_equal(res$replicate_avg, modelGeneExpression_test_replicate_avg)
 })
