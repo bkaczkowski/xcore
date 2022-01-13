30284bc30b76b762b4e4d00d483b1a5cb72c4b9a
Migdal <mcjmigdal@gmail.com>
2022-01-13 12:13:06 +0100

    update R/data.R

--- R/data.R
+++ R/data.R
@@ -13,15 +13,15 @@
 
 #' xcore example molecular signatures
 #'
-#' Molecular signatures data intended for use in xcore vignette and examples. 
-#' It is build ReMap2020 molecular signatures constructed against FANTOM5 
-#' annotation, which can be found in \code{\link[xcoredata]{xcoredata}} package. 
-#' Here the data is only a subset limited to core promoters 
+#' Molecular signatures data intended for use in xcore vignette and examples.
+#' It is build ReMap2020 molecular signatures constructed against FANTOM5
+#' annotation, which can be found in \code{{xcoredata} package.
+#' Here the data is only a subset limited to core promoters
 #' (\code{promoters_f5_core}) and randomly selected 600 signatures.
 #'
-#' @format A \code{dgCMatrix} with 14191 rows and 600 columns holding 
-#'   interaction matrix for subset of ReMap2020 molecular signatures against 
-#'   FANTOM5 annotation. Rows corresponds to FANTOM5 promoters and columns to 
+#' @format A \code{dgCMatrix} with 14191 rows and 600 columns holding
+#'   interaction matrix for subset of ReMap2020 molecular signatures against
+#'   FANTOM5 annotation. Rows corresponds to FANTOM5 promoters and columns to
 #'   signatures.
 #'
 "remap_mini"
