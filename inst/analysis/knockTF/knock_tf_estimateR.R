53efe9ece2e3a382a8ebeee70c369e83087f6f64
Migdal <mcjmigdal@gmail.com>
2022-06-16 23:14:50 +0200

    estimate R script

---
+++ inst/analysis/knockTF/knock_tf_estimateR.R
@@ -0,0 +1,112 @@
+#!/usr/bin/env R
+devtools::load_all()
+remap_promoters_f5 <- xcoredata::remap_promoters_f5()
+promoters_f5_core <- xcoredata::promoters_f5_core()
+
+# UTILS ========================================================================
+topOrSigRes <- function(res) {
+  nsig <- sum(abs(res[["z_score"]]) > 1.95)
+  if (nsig >= 10) {
+    head(res, nsig)
+  } else {
+    head(res, 10)
+  }
+}
+
+getRank <- function(res, TF) {
+  m <- grep(TF, res$name)
+  ifelse(length(m), m[1], 0)
+}
+
+baseName <- function(x, suffix = "") {
+        x %>% 
+        sub(pattern = ".*/", replacement = "") %>%
+        sub(pattern = suffix, replacement = "")
+}
+
+symbol2fantom <- setNames(promoters_f5_core$name, promoters_f5_core$SYMBOL)
+
+processCounts <- function(in_counts) {
+  counts_symbol <- in_counts[, -1] %>% as.matrix() %>% type.convert(as.is = TRUE)
+  rownames(counts_symbol) <- in_counts[, 1]
+  counts_symbol <-
+    counts_symbol[rownames(counts_symbol) %in% promoters_f5_core$SYMBOL, ]
+  rownames(counts_symbol) <- symbol2fantom[rownames(counts_symbol)]
+  
+  return(counts_symbol)
+}
+
+makeDesign <- function(mat) {
+  cond <-
+    ifelse(grepl(pattern = "k", x = colnames(mat)),
+           yes = "knockdown",
+           no = "control")
+  ncond <- cond %>% unique() %>% length()
+  design <- diag(ncond)[cond %>% factor() %>% as.numeric(), ]
+  rownames(design) <- colnames(mat)
+  colnames(design) <- cond %>% factor() %>% levels()
+  
+  return(design)
+}
+
+makeMAE <- function(counts_symbol, ex_design) {
+  design_wo_base <- ex_design
+  design_wo_base[, "control"] <- 0
+  mae <- MultiAssayExperiment(
+    experiments = MultiAssayExperiment::ExperimentList(
+      U = matrix(
+        data = rowMeans(counts_symbol[, ex_design[, "control"] == 1, drop = FALSE]),
+        ncol = 1,
+        dimnames = list(rownames(counts_symbol), "u")
+      ),
+      Y = counts_symbol[, ex_design[, "control"] == 0, drop = FALSE]
+    ),
+    metadata = list(design = design_wo_base)
+  )
+  
+  return(mae)
+}
+
+# ==============================================================================
+
+path_to_files <- system.file("inst", "extdata", "knockTF", package = "xcore")
+dump_isilon <- "/mnt/restricted/isihome/mmigdal/projects/knockTF/"
+
+argv <- commandArgs(trailingOnly=TRUE) %>% as.numeric()
+
+results <- list()
+in_files <-
+  list.files(
+    path = path_to_files,
+    pattern = "DataSet_02.*.txt",
+    full.names = TRUE
+  )
+samples <- baseName(in_files, ".txt")
+
+doMC::registerDoMC(2L)
+rsq_list <- list()
+for (i in seq(argv[1], argv[2])) {
+  set.seed(314159265)
+  in_counts <- read.table(file = in_files[i], header = TRUE)
+  counts_symbol <- processCounts(in_counts)
+  ex_design <- makeDesign(counts_symbol)
+  mae <- makeMAE(counts_symbol, ex_design)
+  
+  mae <- addSignatures(mae, remap = remap_promoters_f5)
+  mae <- filterSignatures(mae, min = 0.05, max = 0.95)
+  load(paste0(dump_isilon, samples[i], ".rda")) # gives res
+  cols <- colnames(mae[["Y"]])
+  stat <- foreach(i = seq_along(cols), .combine = c) %dopar%
+    estimateStat(
+      x = mae[["remap"]],
+      y = mae[["Y"]][, i],
+      u = mae[["U"]],
+      s = res$regression_models$remap[[i]]$lambda.min,
+      statistic = rsq)
+  rsq_list[[samples[i]]] <- stat
+}
+
+save(rsq_list, file = paste0("rsq_list_", argv[1], "_", argv[2], ".rda"))
+
+
+
